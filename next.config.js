821826c4f18dab99aede4fd3c0df47b936931488
colins-uky <colin.schuh@uky.edu>
2024-01-26 15:46:26 -0500

    Bug fix 1.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
+    basePath: '/out',
+    assetPrefix: '/out/',
 }
 
 module.exports = nextConfig
--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -11,7 +11,7 @@ const TopBar: React.FC<TopBarProps> = ({ title }) => {
     return (
         <div className="top-bar container-center bg-midnight shadow-lg text-white p-4 w-full h-[10%]" >
             <div className="flex flex-row">
-                <Image className="w-auto h-auto" src={kora_logo} alt="KORA Logo" width={100} height={50}/>
+                <img className="w-1/3 h-auto" src={kora_logo} alt="KORA Logo"/>
                 <h1 className="text-4xl italic font-bold ml-3"> {title} </h1>
             </div>
         </div>
